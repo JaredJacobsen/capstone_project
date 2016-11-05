ce37cddbf7fdb78329410513f7e3b679a0473108
Jared Jacobsen <jaredjacobsen@Jareds-MacBook-Pro.local>
2016-11-05 13:36:32 -0700

    react setup

---
+++ README.md
@@ -0,0 +1,5 @@
+Capstone Project for Galvanize Data Science Immersive
+
+This is a web server that predicts protein allergenicity.
+
+Author: Jared Jacobsen
---
+++ components/Application.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class Application extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <h1>Hello</h1>
+    );
+  }
+}
+
+export default Application;
