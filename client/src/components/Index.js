b782469225d0ae236dbecdc9bf477bc7e60f43ed
JaredJacobsen <Jared@Erlings-Mac-mini.local>
2016-11-16 07:06:09 -0800

    added graphs to allergen modelling

--- client/src/components/Index.js
+++ client/src/components/Index.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
+import { Jumbotron, Button } from 'react-bootstrap'
+
+// const divStyle = {
+//   backgroundImage: 'url(' + '../public/wheat.jpg' + ')',
+//   backgroundSize: 'cover'
+// };
 
 class Index extends Component {
 
   render() {
     return (
       <div>
-        <h1>Welcome to my protein classifier app</h1>
+        <Jumbotron>
+          <h1>A Protein Allergen Classifier</h1>
+          <p>An advanced machine learning model that predicts the allergenicity of proteins</p>
+          <p><Link to='client/dist/allergen'><Button bsStyle="primary" >Make Predictions</Button></Link></p>
+        </Jumbotron>
       </div>
     )
   }
