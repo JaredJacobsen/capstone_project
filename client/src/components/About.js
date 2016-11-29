702b3b91bc07073f22c1e0c373e4a82fbca7a531
JaredJacobsen <Jared@Erlings-Mac-mini.local>
2016-11-29 14:10:59 -0800

    completed front end

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,23 +3,31 @@ import ReactBootstrapSlider from 'react-bootstrap-slider'
 
 class About extends Component {
 
-  changeValue() {
-    console.log("value changed");
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentValue: 0
+    }
+  }
+
+  changeValue(e) {
+    console.log("value changed", this.state.currentValue);
+    this.setState({currentValue: e.target.value})
   }
 
   render() {
     return (
-      <div>
+      <div style={{marginLeft: '100px'}}>
         <h1>Coming Soon</h1>
         <ReactBootstrapSlider
-          value={0}
-          change={this.changeValue}
-          step={1}
-          max={100}
-          min={10}
-          orientation="vertical"
+          value={this.state.currentValue}
+          change={this.changeValue.bind(this)}
+          step={0.005}
+          max={1}
+          min={0}
+          orientation="horizontal"
           reverse={true}
-          disabled="disabled" />
+           />
       </div>
     )
   }
--- minimal_app/application.py
+++ minimal_app/application.py
@@ -8,40 +8,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 
-# def add_protein_characteristics(df):
-#     df = df.copy()
-#     aa_list = ['A', 'C','E','D','G','F','I','H','K','M','L','N','Q','P','S','R','T','W','V','Y']
-#     aa_dict = {}
-#     for aa in aa_list:
-#         aa_dict[aa] = []
-#     prop_dict = {'aromaticity': [], 'helix': [], 'turn': [], 'sheet': [], 'isoelectric_point': [], 'gravy': []} #, 'flexibility': [], 'instability_index': []}
-#     for i, s in enumerate(df['sequence']):
-#         s = s.replace('B', 'D').replace('Z', 'E').replace('J', 'L').replace('X', 'G').replace('U', 'C').replace('O', 'K')
-#         pa = ProteinAnalysis(s)
-#         prop_dict['aromaticity'].append(pa.aromaticity())
-#         prop_dict['isoelectric_point'].append(pa.isoelectric_point())
-#         prop_dict['gravy'].append(pa.gravy())
-#         # prop_dict['instability_index'].append(pa.instability_index())
-#         # prop_dict['flexibility'].append(np.mean(pa.flexibility()))
-#         for fraction, ss in zip(pa.secondary_structure_fraction(), ['helix', 'turn', 'sheet']):
-#             prop_dict[ss].append(fraction)
-#         for k, v in pa.get_amino_acids_percent().items():
-#             aa_dict[k].append(v)
-#     for k, v in aa_dict.items():
-#         df[k] = v
-#     for k, v in prop_dict.items():
-#         df[k] = v
-#     return df
-#
-#
-# def protein_input_to_rf_pred_df(text, model):
-#     entries = text.split()
-#     df = pd.DataFrame(data=entries, columns=['sequence'])
-#     X = add_protein_characteristics(df)
-#     X = X.drop(['sequence'], axis=1)
-#     df['prediction'] = model.predict(X)
-#     return df
-
 def input_to_nb_pred_df(text, model):
     entries = text.split()
     df = pd.DataFrame(data=entries, columns=['sequence'])
@@ -59,6 +25,6 @@ def predict_allergens():
     return df.T.to_json()
 
 if __name__ == "__main__":
-    with open('allergen_model.pkl', 'r') as fin:
+    with open('./allergen_model.pkl', 'r') as fin:
         allergen_model = pickle.load(fin)
     application.run()
